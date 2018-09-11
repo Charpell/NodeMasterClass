5e9e41ff76b1b94619af87ddd1a22c0a0238bf74
Charpell <charpellumeh@gmail.com>
2018-09-11 15:18:24 +0100

    Routing Request

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 /**
  * Primary file for the API
-*/
+ */
 
-var http = require('http');
-var url = require('url');
-var StringDecoder = require('string_decoder').StringDecoder;
-
+var http = require("http");
+var url = require("url");
+var StringDecoder = require("string_decoder").StringDecoder;
 
 var server = http.createServer(function(req, res) {
-
   // Get the HTTP Method
   var method = req.method.toLowerCase();
 
@@ -23,34 +21,83 @@ var server = http.createServer(function(req, res) {
 
   // Get the path
   var path = parsedUrl.pathname;
-  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  var trimmedPath = path.replace(/^\/+|\/+$/g, "");
 
   // Get the payload, if any
-  var decoder = new StringDecoder('utf-8');
-  var buffer = '';
+  var decoder = new StringDecoder("utf-8");
+  var buffer = "";
 
   // Stream in a payload that is incoming
-  req.on('data', function(data) {
-    buffer += decoder.write(data)
-  })
+  req.on("data", function(data) {
+    buffer += decoder.write(data);
+  });
 
   // End Streaming
-  req.on('end', function() {
+  req.on("end", function() {
     buffer += decoder.end();
 
-    // Send the response
-    res.end('Hello World\n');
+    console.log(trimmedPath)
 
-    // Log the request path
-    console.log('The request received on path: '+trimmedPath+ ' with method: '+method+' and with these query string parameters', queryStringObject);
-    console.log('Headers', headers)
-    console.log('Payload', buffer)
-  })
+    // Choose the handler this request should go to. If one is not found, use the notFound handler
+    var choosenHandler =
+      typeof router[trimmedPath] !== "undefined"
+        ? router[trimmedPath]
+        : handlers.notFound;
+
+    // Construct the data object to send to the handler
+    var data = {
+      trimmedPath: trimmedPath,
+      queryStringObject: queryStringObject,
+      method: method,
+      headers: headers,
+      payload: buffer
+    };
+
+    choosenHandler(data, function(statusCode, payload) {
+      // Use the status code called back by the handler, or default to 200
+      statusCode = typeof statusCode == "number" ? statusCode : 200;
+
+      // Use the payload called back by the handler, or default to an empty object
+      payload = typeof payload == "object" ? payload : {};
+
+      // Convert the payload to a String
+      var payloadString = JSON.stringify(payload);
+
+      // Return the response
+      res.setHeader("Content-Type", "application/json");
+      res.writeHead(statusCode);
+      res.end(payloadString);
+
+      // Log the request path
+      console.log("Returning this response: ", statusCode, payloadString);
+    });
+  });
+});
+
+server.listen(3000, function() {
+  console.log("The server is listening on port 3000 now");
 });
 
 
-server.listen(3000, function(){
-  console.log('The server is listening on port 3000 now')
-})
 
-// curl localhost:3000/fogo/bar?foo=buzz
+
+
+
+// Define the handlers
+var handlers = {};
+
+// Sample handlers
+handlers.sample = function(data, callback) {
+  // Callback a http status code and a payload object
+  callback(406, { name: "sample handler" });
+};
+
+// Not found handler
+handlers.notFound = function(data, callback) {
+  callback(404);
+};
+
+// Define a request router
+var router = {
+  sample: handlers.sample
+};
